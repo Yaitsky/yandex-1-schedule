47ea6e3da513a3db78308dfa6b47c57be553e81f
Igor Yaitsky <yaickiu@gmail.com>
2017-04-02 17:44:31 +0300

    readme test

--- source/js/templates/lecture-future.js
+++ source/js/templates/lecture-future.js
@@ -1,15 +1,15 @@
-var Handlebars = require("handlebars");module.exports = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+var Handlebars = require("handlebars");module.exports = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
   return "<div class=\"schedule__row\">\n    <div class=\"schedule__col schedule__col--blue schedule__col--wide\">"
-    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
+    + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
     + "</div>\n    <div class=\"schedule__col schedule__col--medium\">"
-    + alias4(((helper = (helper = helpers.school || (depth0 != null ? depth0.school : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"school","hash":{},"data":data}) : helper)))
+    + alias3(((helper = (helper = helpers.school || (depth0 != null ? depth0.school : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"school","hash":{},"data":data}) : helper)))
     + "</div>\n    <div class=\"schedule__col schedule__col--blue\">"
-    + alias4(((helper = (helper = helpers.teacher || (depth0 != null ? depth0.teacher : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"teacher","hash":{},"data":data}) : helper)))
+    + alias3(((helper = (helper = helpers.teacher || (depth0 != null ? depth0.teacher : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"teacher","hash":{},"data":data}) : helper)))
     + "</div>\n    <div class=\"schedule__col\">"
-    + alias4(((helper = (helper = helpers.classroom || (depth0 != null ? depth0.classroom : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"classroom","hash":{},"data":data}) : helper)))
+    + alias3(((helper = (helper = helpers.classroom || (depth0 != null ? depth0.classroom : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"classroom","hash":{},"data":data}) : helper)))
     + "</div>\n    <div class=\"schedule__col\">"
-    + alias4(((helper = (helper = helpers.date || (depth0 != null ? depth0.date : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"date","hash":{},"data":data}) : helper)))
+    + alias3(((helper = (helper = helpers.date || (depth0 != null ? depth0.date : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"date","hash":{},"data":data}) : helper)))
     + "</div>\n</div>";
 },"useData":true});
--- source/js/templates/lecture-last.js
+++ source/js/templates/lecture-last.js
@@ -1,21 +1,21 @@
-var Handlebars = require("handlebars");module.exports = Handlebars.template({"1":function(container,depth0,helpers,partials,data,blockParams) {
+var Handlebars = require("handlebars");module.exports = Handlebars.template({"1":function(depth0,helpers,partials,data,blockParams) {
     return "              <span>"
-    + container.escapeExpression(container.lambda(blockParams[0][0], depth0))
+    + this.escapeExpression(this.lambda(blockParams[0][0], depth0))
     + "</span>\n";
-},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data,blockParams) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data,blockParams) {
+    var stack1, helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
   return "<div class=\"schedule__row\">\n    <div class=\"schedule__col schedule__col--wide\">\n        <a href=\""
-    + alias4(((helper = (helper = helpers.video || (depth0 != null ? depth0.video : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"video","hash":{},"data":data,"blockParams":blockParams}) : helper)))
+    + alias3(((helper = (helper = helpers.video || (depth0 != null ? depth0.video : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"video","hash":{},"data":data,"blockParams":blockParams}) : helper)))
     + "\" class=\"schedule__link\">"
-    + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data,"blockParams":blockParams}) : helper)))
+    + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data,"blockParams":blockParams}) : helper)))
     + "</a>\n    </div>\n    <div class=\"schedule__col schedule__col--medium\">"
-    + alias4(((helper = (helper = helpers.school || (depth0 != null ? depth0.school : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"school","hash":{},"data":data,"blockParams":blockParams}) : helper)))
+    + alias3(((helper = (helper = helpers.school || (depth0 != null ? depth0.school : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"school","hash":{},"data":data,"blockParams":blockParams}) : helper)))
     + "</div>\n    <div class=\"schedule__col\">\n"
-    + ((stack1 = helpers.each.call(alias1,(depth0 != null ? depth0.teacher : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 1, blockParams),"inverse":container.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
+    + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.teacher : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 1, blockParams),"inverse":this.noop,"data":data,"blockParams":blockParams})) != null ? stack1 : "")
     + "    </div>\n    <div class=\"schedule__col\">"
-    + alias4(((helper = (helper = helpers.classroom || (depth0 != null ? depth0.classroom : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"classroom","hash":{},"data":data,"blockParams":blockParams}) : helper)))
+    + alias3(((helper = (helper = helpers.classroom || (depth0 != null ? depth0.classroom : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"classroom","hash":{},"data":data,"blockParams":blockParams}) : helper)))
     + "</div>\n    <div class=\"schedule__col\">\n        <a href=\""
-    + alias4(((helper = (helper = helpers.video || (depth0 != null ? depth0.video : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"video","hash":{},"data":data,"blockParams":blockParams}) : helper)))
+    + alias3(((helper = (helper = helpers.video || (depth0 != null ? depth0.video : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"video","hash":{},"data":data,"blockParams":blockParams}) : helper)))
     + "\" class=\"schedule__button\">Смотреть</a>\n    </div>\n</div>";
 },"useData":true,"useBlockParams":true});
